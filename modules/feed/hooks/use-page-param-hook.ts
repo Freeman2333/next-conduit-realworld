28578cff67808178a127de269a386426d450f259
Serhii190 <serhii@himumsaiddad.com>
2023-06-02 12:02:11 +0300

    use page param hook added

---
+++ modules/feed/hooks/use-page-param-hook.ts
@@ -0,0 +1,22 @@
+import { useSearchParams, useRouter, usePathname } from 'next/navigation';
+
+export const usePageParam = () => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const page = searchParams.get('page') ? Number(searchParams.get('page')) : 0;
+  console.log(usePathname());
+
+  const current = new URLSearchParams(searchParams);
+
+  const setPage = (page: number) => {
+    current.set('page', String(page));
+    const search = current.toString();
+    const query = search ? `?${search}` : '';
+
+    router.push(`${pathname}${query}`);
+  };
+
+  return { page, setPage };
+};
